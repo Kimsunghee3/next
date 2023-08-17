09b3733417d8908a02de15515988244a4a65692f
chopssal <koalasunghee@gmail.com>
2023-08-18 00:17:56 +0900

    Feat: 레이아웃 생성

--- src/app/page.js
+++ src/app/page.js
@@ -3,7 +3,9 @@ import Image from 'next/image'
 export default function Home() {
   return (
     <>
-      hello, nextjs!
+      <h2>welcome</h2>
+      hello web
+      <br></br> <img src="/icon.png"></img>
     </>
   )
 }
